76f752dfd5c678585a63a6e0380612a80f2766c8
Dave Redmin <david.redmin@beta.dhs.gov>
2020-11-12 16:45:31 -0500

    Modify to support account bootstrapping process

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
-# Default provider information.
+# This is the "default" provider that is used to create resources
+# inside the COOL PCA account
 provider "aws" {
+  region = var.aws_region
+  # Use this profile once the account has been bootstrapped.
+  profile = "cool-pca-provisionaccount"
+  # Use this profile, defined using programmatic credentials for
+  # AWSAdministratorAccess as obtained for the COOL PCA account
+  # from the AWS SSO page, to bootstrap the account.
+  # profile = "cool-pca-account-admin"
 }
